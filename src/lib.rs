e8292dd06ac72ba6bb71e5db73f116bc305f66ef
hamadu <dercll1020@gmail.com>
2018-02-13 07:41:26 +0900

    build rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+    let x = ["Hello", "World", "!"];
+    let y = x;
+    let test = (0..3).flat_map(|x| 0..x).zip(0..);
+}
 
 #[lang = "eh_personality"]
 #[no_mangle]
