ce5424254e13a428f032b079bc1479f091efad0c
hamadu <dercll1020@gmail.com>
2018-02-14 23:20:58 +0900

    find address range

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,36 @@
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() -> ! {
-  vga_buffer::clear_screen();
-  println!("{}", { println!("inner"); "outer" });
+pub extern fn rust_main(multiboot_information_address: usize) -> ! {
+  let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+  let memory_map_tag = boot_info.memory_map_tag().expect("failed to obtain memory map tag");
+
+  println!("memory areas:");
+  for area in memory_map_tag.memory_areas() {
+    println!(" start: 0x{:x>8}, length: 0x{:x>8}", area.base_addr, area.length);
+  }
+
+  let elf_sections_tag = boot_info.elf_sections_tag().expect("failed to obtain kernel sections tag");
+  println!("kernel sectinos:");
+  for section in elf_sections_tag.sections() {
+    println!(" start: 0x{:x>8}, length: 0x{:x>8}, flags: 0x{:x}",
+      section.addr, section.size, section.flags);
+  }
+
+  let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+  let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
+  println!("kernel_start: 0x{:x>8}, kernel_end: 0x{:x>8}", kernel_start, kernel_end);
+
+  let multiboot_start = multiboot_information_address;
+  let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+  println!("multiboot_start: 0x{:x>8}, multiboot_end: 0x{:x>8}", multiboot_start, multiboot_end);
+
   loop {}
 }
 
@@ -23,6 +45,8 @@ pub extern fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt() -> ! {
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+  println!("HAMOOOOOON:\nF:{}\nL:{}", file, line);
+  println!("{}", fmt);
   loop {}
 }
